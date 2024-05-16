97c559ff2b27d49085646579684f1deeb16c3577
TimaChumacov <timchumakov@gmail.com>
2024-05-16 18:42:05 +0200

    still not done with crosshair

--- src/general/components.rs
+++ src/general/components.rs
@@ -1,4 +1,6 @@
 use bevy::prelude::*;
 
+pub const ZOOM: f32 = 0.5;
+
 #[derive(Component)] 
 pub struct Crosshair {}
--- src/general/systems.rs
+++ src/general/systems.rs
@@ -1,6 +1,6 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, window};
 use bevy::window::PrimaryWindow;
-use super::components::Crosshair;
+use super::components::{Crosshair, ZOOM};
 
 pub fn spawn_camera(
     mut commands: Commands,
@@ -13,8 +13,8 @@ pub fn spawn_camera(
             window.height() / 2.0,
             0.0,
         ).with_scale(Vec3::new(
-            0.5,
-            0.5,
+            ZOOM,
+            ZOOM,
             1.0,
         )),
         ..default()
@@ -36,8 +36,20 @@ pub fn spawn_crosshair(
 
 pub fn crosshair_follow_mouse(
     mut crosshair_query: Query<&mut Transform, With<Crosshair>>,
-    window_query: Query<&Window, With<PrimaryWindow>>
+    mut window_query: Query<&mut Window, With<PrimaryWindow>>,
 ) {
+    // let cursor_position = window_query.single().cursor_position();
+    // if cursor_position.is_some() {
+    //     let mut crosshair_transform = crosshair_query.single_mut();
+    //     crosshair_transform.translation = cursor_position.unwrap().extend(0.0)
+    // }
+    let mut window = window_query.get_single().unwrap();
+    //window.cursor.visible = false;
+    let cursor_position = window_query.single().cursor_position().unwrap_or_default();
     let mut crosshair_transform = crosshair_query.single_mut();
-    crosshair_transform.translation = window_query.single().cursor_position().unwrap().extend(0.0); 
+    crosshair_transform.translation = Vec3::new(
+        (cursor_position.x - window.width() / 2.0) * ZOOM + window.width() / 2.0, 
+        (cursor_position.y - window.height() / 2.0) * ZOOM + window.height() / 2.0, 
+        0.0
+    );
 }
